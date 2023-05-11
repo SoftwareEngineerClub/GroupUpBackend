a52bc4d24770df9c9adbab5817ea1a894aa8fdd3
Noel <no3lcodes@gmail.com>
2023-05-11 16:07:59 -0700

    first commit

---
+++ src/main/java/SoftwareEngineerClub/GroupUpBackend/GroupUpBackendApplication.java
@@ -0,0 +1,13 @@
+package SoftwareEngineerClub.GroupUpBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupUpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupUpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SoftwareEngineerClub/GroupUpBackend/GroupUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SoftwareEngineerClub.GroupUpBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupUpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
